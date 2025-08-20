72cb7b41f738a4797702a8655cc7b9b962f9f393
wonwon115 <wonwon.000050@gmail.com>
2025-08-20 09:27:18 -0300

    20/8

--- clase 0/main.js
+++ clase 0/main.js
@@ -11,9 +11,23 @@
  */
 // opcion #3 creando una funcuin flecha o arrow 
 
-let nombre = prompt (" ingrese su nombre")
+/* let nombre = prompt (" ingrese su nombre")
 
-const saludar = () => {
-    alert("Hola " + nombre)
+const saludar = (name) => {
+    alert("Hola " + name)
 }
-saludar()
+saludar(nombre) */
+ 
+function calcular(valor1, valor2){
+    return valor1 + valor2
+}
+let resultado = calcular (10, 20 )
+console.log("El resultado es: " + resultado) // = 30
+console.log("El resultado es: " + calcular(20, 30)) // = 50
+
+function mostrarMensaje(){
+    let contenido ='<div class="alert alert-success" role="alert"> + mensaje + </div>'
+
+    document.getElementById("mensaje").innerHTML = contenido
+}
+mostrarMensaje("Estamos cursando React JS en CoderHouse")
